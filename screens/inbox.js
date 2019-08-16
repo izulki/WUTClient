d5d4cd00c812d1080d50720878803d8e9090951d
Irfan <maulana@dal.ca>
2019-08-16 14:37:53 -0300

    basic flat list structure

--- screens/inbox.js
+++ screens/inbox.js
@@ -1,30 +1,68 @@
 import React, {Fragment} from 'react';
-import {View, Text} from 'react-native'
+import {View, Text, FlatList} from 'react-native';
+const GLOBAL = require('../globals');
 
-class InboxScreen extends React.Component {
-    constructor(props) {
-        super(props);
+import {List, ListItem} from 'react-native-elements';
 
-        this.state = {
-            userData: this.props.navigation.getParam("userData", null),
-        }
+export default class InboxScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-    }
-
-    state = {
-        userData: null,
-    }
-    componentDidMount() {
-        console.log(this.state.userData)
-    }
-
-    render() {
-        return (
-          <View>
-              <Text>{this.state.userData[0].user_id}</Text>
-          </View>
-        );
-      }
+    this.state = {
+      loading: true,
+      messages: [],
+    };
   }
 
-export default InboxScreen;
+  componentDidMount() {
+    this._fetchInbox(this.props.navigation.getParam('userData', null));
+  }
+
+  _fetchInbox(userData) {
+
+    console.log(
+      `_fetchInbox(): ${GLOBAL.API_ADDRESS_PORT}email/${userData[0].user_id}`,
+    );
+    fetch(`${GLOBAL.API_ADDRESS_PORT}email/${userData[0].user_id}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length) {
+          this.setState({messages: data, loading: false});     
+        } else {
+          alert('No messages.');
+          this.setState({loading: false});
+        }
+      });
+  }
+
+  _fetchUserInfo(id){
+    console.log(
+        `_fetchUserInfo(): ${GLOBAL.API_ADDRESS_PORT}email/user/${id}`,
+      );
+    fetch(`${GLOBAL.API_ADDRESS_PORT}email/user/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length) {
+            return `${data[0].first_name} ${data[0].last_name}`
+        } else {
+          alert('No Such User');
+        }
+      });
+  }
+
+  render() {
+    return (
+      <View>
+         {
+        this.state.messages.map((l, i) => (
+            <ListItem
+                key={l.id} //string?
+                title={`${l.first_name}`}
+                subtitle={l.message}
+            />
+        ))
+        }
+      </View>
+    );
+  }
+}
