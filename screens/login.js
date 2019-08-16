d9171acfa34a16746e0ba9fe9d88bb28b3419ea9
Irfan <maulana@dal.ca>
2019-08-16 11:44:13 -0300

    Some login styling

--- screens/login.js
+++ screens/login.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Text, TextInput, Button} from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 const GLOBAL = require('../globals');
 
 export default class Login extends React.Component {
   state = {
-    address: "null",
+    address: 'null',
   };
 
   _login() {
@@ -13,21 +13,49 @@ export default class Login extends React.Component {
       .then(response => response.json())
       .then(data => {
         if (data.length) {
-            alert("Success");
+          alert('Success');
         } else {
-            alert("Account does not exist.");
+          alert('Account does not exist.');
         }
       });
   }
 
   render() {
     return (
-      <View>
-        <Text>WeUsThem</Text>
-        <Text>Email Client</Text>
-        <TextInput title="Enter Email Address" onChangeText={(text) => {this.setState({address: text})}} />
-        <Button title={'Login'} onPress={() => this._login()} />
+      <View style={styles.loginWrapper}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+          }}>
+          <Text style={styles.WUTTitle}>WeUsThem</Text>
+          <Text style={styles.WUTSubtitle}>Email Client</Text>
+          <TextInput
+            placeholder = {"Enter Email Address"}
+            textAlign = {"center"}
+            onChangeText={text => {
+              this.setState({address: text});
+            }}
+          />
+          <Button title={'Login'} onPress={() => this._login()} />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  loginWrapper: {
+    flex: 1,
+    alignContent: 'center',
+    marginHorizontal: "10%",
+    marginBottom: "40%"
+  },
+  WUTTitle: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  WUTSubtitle: {
+    textAlign: 'center',
+  },
+});
